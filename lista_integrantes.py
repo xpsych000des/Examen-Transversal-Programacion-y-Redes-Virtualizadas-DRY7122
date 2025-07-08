69a7f581cfe43fb7e18254e0afa5fc803ae72f1f
xpsych000des <mzjanic0nfig.ps1@gmail.com>
2025-07-08 05:06:21 +0000

    Script en Python que imprime una lista con los nombres y apellidos de los integrantes del grupo

---
+++ lista_integrantes.py
@@ -0,0 +1,7 @@
+# Integrantes del Grupo
+integrantes = ["Janice Pereira Uribe", "Leonardo Quintana Villarroel"]
+
+# Muestra en el Terminal la Lista de Integrantes
+print("Integrantes del Grupo:")
+for integrante in integrantes:
+    print(integrante)
